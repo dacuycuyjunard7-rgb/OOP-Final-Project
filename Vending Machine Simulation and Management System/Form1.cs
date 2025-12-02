add7461866abd383590f0bebbd55e4cb3e0e75be
JunardDacuycuy <JunardDacuycuy@SMU-0056>
2025-12-02 12:11:45 +0800

    Add project files.

---
+++ Vending Machine Simulation and Management System/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vending_Machine_Simulation_and_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
